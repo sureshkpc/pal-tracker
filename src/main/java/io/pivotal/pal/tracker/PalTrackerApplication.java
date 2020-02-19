42cc4e14dda5a90a985452232d52f8e41cc1ba21
ashwinkumar0211 <ashwinkumar02@yahoo.com>
2020-02-19 14:22:52 +0530

    Database operations

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -3,15 +3,19 @@ package io.pivotal.pal.tracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import io.pivotal.pal.tracker.JdbcTimeEntryRepository;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class PalTrackerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
-    }
+            }
+
     @Bean
-    TimeEntryRepository timeEntryRepository(){
-        return new InMemoryTimeEntryRepository();
+    TimeEntryRepository timeEntryRepository( DataSource dataSource  ){
+        return new JdbcTimeEntryRepository(dataSource);
     }
 }
